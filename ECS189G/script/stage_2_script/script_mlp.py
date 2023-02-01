190bb15902cb1f77837abe2d81efc036ab9e0618
hongye_xu <114460759+hyexu1608@users.noreply.github.com>
2023-02-01 00:14:21 -0800

    Add files via upload

---
+++ ECS189G/script/stage_2_script/script_mlp.py
@@ -0,0 +1,47 @@
+from code.stage_2_code.Dataset_Loader import Dataset_Loader
+from code.stage_2_code.Method_MLP import Method_MLP
+from code.stage_2_code.Result_Saver import Result_Saver
+from code.stage_2_code.Evaluate_Accuracy import Evaluate_Accuracy
+from code.stage_2_code.Setting_partitionedData import Setting_partitionedData
+import numpy as np
+import torch
+
+#---- Multi-Layer Perceptron script ----
+if 1:
+    #---- parameter section -------------------------------
+    np.random.seed(2)
+    torch.manual_seed(2)
+    #------------------------------------------------------
+
+    # ---- objection initialization setction ---------------
+    data_obj = Dataset_Loader('train data and test data', '')
+    data_obj.train_dataset_source_folder_path = '../../data/stage_2_data/'
+    data_obj.train_dataset_source_file_name = 'train.csv'
+
+    data_obj.test_dataset_source_folder_path = '../../data/stage_2_data/'
+    data_obj.test_dataset_source_file_name = 'test.csv'
+
+    method_obj = Method_MLP('multi-layer perceptron', '')
+
+    result_obj = Result_Saver('saver', '')
+    result_obj.result_destination_folder_path = '../../result/stage_2_result/MLP_'
+    result_obj.result_destination_file_name = 'prediction_result'
+
+    setting_obj = Setting_partitionedData('already partitioned', '')
+
+
+    evaluate_obj = Evaluate_Accuracy('accuracy', '')
+    # ------------------------------------------------------
+
+    # ---- running section ---------------------------------
+    print('************ Start ************')
+    setting_obj.prepare(data_obj, method_obj, result_obj, evaluate_obj)
+    setting_obj.print_setup_summary()
+    mean_score, std_score = setting_obj.load_run_save_evaluate()
+    print('************ Overall Performance ************')
+    print('MLP Accuracy: ' + str(mean_score) + ' +/- ' + str(std_score))
+    print('************ Finish ************')
+    # ------------------------------------------------------
+    
+
+    
