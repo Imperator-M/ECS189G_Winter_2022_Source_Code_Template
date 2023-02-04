ba8d716312b88a6470e436be837a164b79f3ec0d
hongye_xu <114460759+hyexu1608@users.noreply.github.com>
2023-02-03 17:54:53 -0800

    Add files via upload

--- ECS189G/code/stage_2_code/Method_MLP.py
+++ ECS189G/code/stage_2_code/Method_MLP.py
@@ -15,7 +15,7 @@ import matplotlib.pyplot as plt
 class Method_MLP(method, nn.Module):
     data = None
     # it defines the max rounds to train the model
-    max_epoch = 50
+    max_epoch = 500
     # it defines the learning rate for gradient descent based optimizer for model learning
     learning_rate = 1e-3
 
@@ -29,9 +29,13 @@ class Method_MLP(method, nn.Module):
         self.fc_layer_1 = nn.Linear(784, 4)
         # check here for nn.ReLU doc: https://pytorch.org/docs/stable/generated/torch.nn.ReLU.html
         self.activation_func_1 = nn.ReLU()
-        self.fc_layer_2 = nn.Linear(4, 10)
+        self.fc_layer_2 = nn.Linear(4, 5)
+        self.activation_func_2 = nn.ReLU()
+        self.fc_layer_3 = nn.Linear(5, 6)
+        self.activation_func_3 = nn.ReLU()
+        self.fc_layer_4 = nn.Linear(6, 10)
         # check here for nn.Softmax doc: https://pytorch.org/docs/stable/generated/torch.nn.Softmax.html
-        self.activation_func_2 = nn.Softmax(dim=1)
+        self.activation_func_4 = nn.Softmax(dim=1)
 
     # it defines the forward propagation function for input x
     # this function will calculate the output layer by layer
@@ -44,7 +48,9 @@ class Method_MLP(method, nn.Module):
         # self.fc_layer_2(h) will be a nx2 tensor
         # n (denotes the input instance number): 0th dimension; 2 (denotes the class number): 1st dimension
         # we do softmax along dim=1 to get the normalized classification probability distributions for each instance
-        y_pred = self.activation_func_2(self.fc_layer_2(h))
+        g = self.activation_func_2(self.fc_layer_2(h))
+        k = self.activation_func_3(self.fc_layer_3(g))
+        y_pred = self.activation_func_4(self.fc_layer_4(k))
         return y_pred
 
     # backward error propagation will be implemented by pytorch automatically
@@ -62,6 +68,7 @@ class Method_MLP(method, nn.Module):
         # we don't do mini-batch, we use the whole input as one batch
         # you can try to split X and y into smaller-sized batches by yourself
         loss_temp = []
+        epoch_temp = []
         for epoch in range(self.max_epoch): # you can do an early stop if self.max_epoch is too much...
             # get the output, we need to covert X into torch.tensor so pytorch algorithm can operate on it
             y_pred = self.forward(torch.FloatTensor(np.array(X)))
@@ -81,10 +88,17 @@ class Method_MLP(method, nn.Module):
 
             if epoch%10 == 0 or epoch == self.max_epoch-1:
                 accuracy_evaluator.data = {'true_y': y_true, 'pred_y': y_pred.max(1)[1]}
-                print('Epoch:', epoch, 'Accuracy:', accuracy_evaluator.evaluate(), 'Loss:', train_loss.item())
+                accuracy, precision, recall, f1 = accuracy_evaluator.evaluate()
+                print('Epoch:', epoch,
+                      'Accuracy:', accuracy,
+                      'precision:', precision,
+                      'recall:', recall,
+                      'f1:', f1,
+                      'Loss:', train_loss.item())
+                epoch_temp.append(epoch)
                 loss_temp.append(train_loss.item())
 
-        plt.plot(range(len(loss_temp)), loss_temp, marker='o')
+        plt.plot(epoch_temp, loss_temp, marker='o')
         plt.xlabel('Epochs')
         plt.ylabel('Loss')
         plt.show()
