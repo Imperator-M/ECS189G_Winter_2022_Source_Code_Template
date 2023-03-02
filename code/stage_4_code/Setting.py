cf416b89549e824237ab1d57a20bb8aeaeb8e8fd
CennyBo <116618080+CennyBo@users.noreply.github.com>
2023-03-01 22:41:45 -0800

    Finished rough preprocessing.

---
+++ code/stage_4_code/Setting.py
@@ -0,0 +1,31 @@
+'''
+Concrete SettingModule class for a specific experimental SettingModule
+'''
+
+# Copyright (c) 2017-Current Jiawei Zhang <jiawei@ifmlab.org>
+# License: TBD
+
+from code.base_class.setting import setting
+import numpy as np
+
+class Setting(setting):
+
+    def load_run_save_evaluate(self):
+        
+        # load dataset
+        loaded_data = self.dataset.load()
+        X_train, X_test = np.array(loaded_data['X_train']), np.array(loaded_data['X_train'])
+        y_train, y_test = np.array(loaded_data['y_train']), np.array(loaded_data['y_train'])
+
+        # run MethodModule
+        self.method.data = {'train': {'X': X_train, 'y': y_train}, 'test': {'X': X_test, 'y': y_test}}
+        learned_result = self.method.run()
+
+        # save raw ResultModule
+        self.result.data = learned_result
+        self.result.save()
+
+        self.evaluate.data = learned_result
+        score = self.evaluate.evaluate()
+
+        return score
