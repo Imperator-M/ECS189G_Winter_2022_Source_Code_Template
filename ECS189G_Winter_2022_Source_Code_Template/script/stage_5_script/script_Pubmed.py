9ba6ffc258563fa07245ae1b3973f275dbe6f70d
Imperator-M <104466672+Imperator-M@users.noreply.github.com>
2023-03-12 20:10:59 -0700

    Add files via upload

---
+++ ECS189G_Winter_2022_Source_Code_Template/script/stage_5_script/script_Pubmed.py
@@ -0,0 +1,47 @@
+from code.stage_5_code.Dataset_Loader import Dataset_Loader
+from code.stage_5_code.Method_Pubmed import Method_Pubmed
+from code.stage_5_code.Result_Saver import Result_Saver
+from code.stage_5_code.Evaluate_Accuracy import Evaluate_Accuracy
+from code.stage_5_code.Setting_Train_Test_Split import Setting_Train_Test_Split
+import numpy as np
+import torch
+
+
+if 1:
+    # ---- parameter section -------------------------------
+    np.random.seed(2)
+    torch.manual_seed(2)
+    # ------------------------------------------------------
+
+    # ---- objection initialization section ---------------
+    data_obj = Dataset_Loader('pubmed', '')
+    data_obj.dataset_source_folder_path = '../../data/stage_5_data/pubmed'
+    data_obj.dataset_source_file_name = 'pubmed'
+
+###############################################################################################
+    method_obj = Method_Pubmed('graphical convolutional network', '')
+
+    result_obj = Result_Saver('saver', '')
+    result_obj.result_destination_folder_path = '../../result/stage_5_result/GCN_'
+    result_obj.result_destination_file_name = 'prediction_result_Pubmed'
+
+    ##################################################################
+    setting_obj = Setting_Train_Test_Split("", '')
+    ##################################################################
+
+    evaluate_obj = Evaluate_Accuracy('accuracy', '')
+    # ------------------------------------------------------
+
+    # ---- running section ---------------------------------
+    print('************ Start ************')
+    setting_obj.prepare(data_obj, method_obj, result_obj, evaluate_obj)
+    setting_obj.print_setup_summary()
+
+    mean_score, std_score = setting_obj.load_run_save_evaluate()
+    print('************ Overall Performance ************')
+    print('GCN Accuracy: ' + str(mean_score[0]))
+    print('GCN Precision: ' + str(mean_score[1]))
+    print('GCN Recall: ' + str(mean_score[2]))
+    print('GCN F1: ' + str(mean_score[3]))
+    print('************ Finish ************')
+    # ------------------------------------------------------
