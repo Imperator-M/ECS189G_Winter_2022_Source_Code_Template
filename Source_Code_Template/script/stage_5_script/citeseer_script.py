3b3024736170b4fdbcb58400e97984a615c30849
username <71690828+UchihaCryptic@users.noreply.github.com>
2023-03-16 20:10:13 -0700

    finishing touches

---
+++ Source_Code_Template/script/stage_5_script/citeseer_script.py
@@ -0,0 +1,46 @@
+from code.stage_5_code.Dataset_Loader import *
+from code.stage_5_code.Citeseer import *
+from code.stage_5_code.Result_Saver import *
+from code.stage_5_code.Evaluate_Accuracy import *
+from code.stage_5_code.Setting_Train_Test_Split import *
+
+import numpy as np
+import torch
+
+if 1:
+    # ---- parameter section -------------------------------
+    np.random.seed(2)
+    torch.manual_seed(2)
+    # ------------------------------------------------------
+
+    # ---- objection initialization section ---------------
+    data_obj = Dataset_Loader('citeseer', '')
+    data_obj.dataset_source_folder_path = '../../data/stage_5_data/citeseer'
+    data_obj.dataset_source_file_name = 'citeseer'
+
+###############################################################################################
+    method_obj = Citeseer_Method('gcn', '')
+
+    result_obj = Result_Saver('saver', '')
+    result_obj.result_destination_folder_path = '../../result/stage_5_result/GCN_'
+    result_obj.result_destination_file_name = 'prediction_result_Citeseer'
+
+    ##################################################################
+    setting_obj = Setting_Train_Test_Split("", '')
+    ##################################################################
+
+    evaluate_obj = Evaluate_Accuracy('accuracy', '')
+    # ------------------------------------------------------
+
+    # ---- running section ---------------------------------
+    print('************ Start ************')
+    setting_obj.prepare(data_obj, method_obj, result_obj, evaluate_obj)
+    setting_obj.print_setup_summary()
+
+    mean_score, std_score = setting_obj.load_run_save_evaluate()
+    print('************ Overall Performance ************')
+    print('GCN Accuracy: ' + str(mean_score[0]))
+    print('GCN Precision: ' + str(mean_score[1]))
+    print('GCN Recall: ' + str(mean_score[2]))
+    print('GCN F1: ' + str(mean_score[3]))
+    print('************ Finish ************')
---
+++ Source_Code_Template/script/stage_5_script/code/stage_5_code/Ablated_PubMed.py
@@ -0,0 +1,86 @@
+# Edited source code provided from PyGCN repo
+
+import torch
+from torch import nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+from code.base_class.method import method
+from code.stage_5_code.Evaluate_Accuracy import *
+from code.stage_5_code.layers import *
+
+import matplotlib.pyplot as plt
+
+class Ablated_PubMed_Method(method, nn.Module):
+    data = None
+    epoch = 500
+    lr = 0.05
+    dr = 0.0003
+    num_input = 500
+    num_dim = 8
+    output = 3
+
+    def __init__(self, mName, mDescription):
+        method.__init__(self, mName, mDescription)
+        nn.Module.__init__(self)
+        # number of features first then number of hidden
+        self.gconv1 = GraphConvolution(self.num_input, self.num_dim)
+        self.gconv2 = GraphConvolution(self.num_dim, self.output)
+        self.dropout = 0.5
+    
+    def forward(self, x, adj):
+        x = F.leaky_relu(self.gconv1(x, adj))
+        #x = F.dropout(x, self.dropout, training=self.training)
+        x = self.gconv2(x, adj)
+        return F.log_softmax(x, dim=1)
+    
+    def trainer(self, graph, train_test_val):
+        self.train()
+        
+        optimizer = optim.Adam(self.parameters(), lr=self.lr, weight_decay=self.dr)
+        optimizer.zero_grad()
+
+        losses = []
+
+        loss_fn = nn.CrossEntropyLoss()
+
+        for epoch in range(self.epoch):
+            output = self(graph["X"], graph["utility"]["A"])
+            loss = loss_fn(output[train_test_val["idx_train"]], graph["y"][train_test_val["idx_train"]])
+            losses.append(loss.item())
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            print("Epoch:", epoch, "Loss:", loss.item())
+        
+
+        #plt.plot(losses)
+        #plt.xlabel("Epoch")
+        #plt.ylabel("Loss")
+        #plt.title("PubMed Training Loss")
+        #plt.savefig("pubmed_training_loss.png")
+        #
+    
+    def test(self, graph, train_test_val):
+        self.eval()
+
+        idx_test = train_test_val["idx_test"]
+
+        with torch.no_grad():
+            output = self(graph["X"], graph["utility"]["A"])
+            _, predicted = torch.max(output, dim=1)
+
+            return predicted[idx_test]
+    
+    def run(self):
+        print('method running...')
+        print('--start training...')
+
+        self.trainer(self.data['graph'], self.data['train_test_val'])
+        print('--start testing...')
+        pred_y = self.test(self.data['graph'], self.data['train_test_val'])
+
+        return {'pred_y': pred_y, 'true_y': self.data['graph']['y'][self.data['train_test_val']['idx_test']]}
+
